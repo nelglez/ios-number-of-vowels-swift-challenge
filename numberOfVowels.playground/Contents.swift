73570941f193b3dccb1485da53c3acfd1dbe126c
Nelson Gonzalez <nelglezfl@gmail.com>
2019-09-09 21:13:20 -0400

    Code challenge finished now.

--- numberOfVowels.playground/Contents.swift
+++ numberOfVowels.playground/Contents.swift
@@ -2,21 +2,19 @@ import UIKit
 
 func numberOfVowels(in string: String, isYAVowel: Bool = false) -> Int {
     
-    // No need to run any code if the string is empty
+    // check to see if the string is empty
     guard !string.isEmpty else { return 0 }
     
-    // Set up the variables
     let vowels: [Character] = isYAVowel ? ["a", "e", "i", "o", "u", "y"] : ["a", "e", "i", "o", "u"]
     var count = 0
     
-    // Loop through each character and check if it is a vowel.
+    // Loop through and check if its a vowel.
     for character in string.lowercased() {
         if vowels.contains(character) {
             count += 1
         }
     }
-    
-    // Return the count
+
     return count
 }
 
